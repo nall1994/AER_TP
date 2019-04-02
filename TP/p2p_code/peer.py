b9714ed6100f18d7a135b17a2d06d97e63437a22
nall1994 <34685054+nall1994@users.noreply.github.com>
2019-04-02 17:18:57 +0100

    upd14

--- TP/p2p_code/peer.py
+++ TP/p2p_code/peer.py
@@ -28,11 +28,8 @@ class Peer:
         lc_thread.start()
         mc_thread.start()
         cml_thread.start()
-        try:
-            mainmenu_thread.start()
-        except SystemExit:
-            print('system exit 2!')
-            sys.exit("Manually exiting P2P network.")
+        mainmenu_thread.start()
+        sys.exit("Manually exiting P2P network.")
 
     # Função de conexão de um peer a 3 known_peers
     def connect(self):
@@ -198,8 +195,7 @@ class Peer:
                 print("\n")
                 print("The choice has to be a number from 1 to 6.")
             except SystemExit:
-                print('system exit 1!')
-                raise SystemExit()
+                break
     
     def conn_peers(self):
         print("- KNOWN PEERS -")
